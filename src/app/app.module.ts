ac89a5c7f4c49aa508b53beeddeb4436329ea4c6
lebs87 <lebs87@gmail.com>
2023-06-12 14:19:42 -0300

    Creando portafolio con Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CurriculumComponent } from './curriculum/curriculum.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    PresentationComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    CurriculumComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
